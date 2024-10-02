c3470c609134f7d16830cf65e41240904d61c6da
Benjamin Martin <000953368@mohawkcollege.ca>
2024-10-02 11:51:50 -0400

    Add background and building

--- martin-ben-a2-2D-interactive-drawing/Game.cs
+++ martin-ben-a2-2D-interactive-drawing/Game.cs
@@ -12,6 +12,13 @@ namespace Game10003
     {
         // Place your variables here:
 
+        // Colours
+        Color navyBlue = new Color(0, 2, 58); 
+        Color brickRed = new Color(130, 23, 0);
+        Color concreateGrey = new Color(142, 144, 118);
+        Color darkGreenBack = new Color(38, 115, 50); 
+        Color darkGreenFront = new Color(45, 124, 64); 
+        
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -27,6 +34,56 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+
+            // Background Buildings
+            Draw.FillColor = Color.Black;
+            Draw.Rectangle(0, 260, 20, 140); 
+            Draw.Rectangle(20, 180, 20, 220); 
+            Draw.Rectangle(360, 260, 20, 140); 
+            Draw.Rectangle(380, 240, 20, 160);
+
+            // Background Bushes
+            DrawBush(0, 390, 25);
+            DrawBush(360, 390, 25);
+
+            // Main Building 
+            Draw.FillColor = brickRed;
+            Draw.Rectangle(40, 100, 320, 300);
+
+            // Roof 
+            Draw.FillColor = concreateGrey;
+            Draw.Rectangle(20, 80, 360, 30);
+
+            // Dark Windows 
+            Draw.FillColor = Color.Black;
+            for (int index = 0; index < 3; index++)
+            {
+                int xOffset = index * 100;
+                Draw.Square(60 + xOffset, 120, 80);
+            }
+            for (int index = 0; index < 3; index++)
+            {
+                int xOffset = index * 100;
+                Draw.Square(60 + xOffset, 220, 80);
+            }
+            for (int index = 0; index < 3; index++)
+            {
+                int xOffset = index * 100;
+                Draw.Square(60 + xOffset, 320, 80);
+            }
+
+
+                Window.ClearBackground(navyBlue);
+        }
+
+        void DrawBush(float x, float y, float radius)
+        {
+            Draw.LineSize = 0;
+            Draw.FillColor = darkGreenBack;
+            Draw.Circle(x + 20, y - 20, radius - 5);
+            Draw.FillColor = darkGreenFront;
+            Draw.Circle(x, y, radius);
+            Draw.Circle(x + 40, y, radius);
 
         }
     }
