74ef922fe59074c83ee1a68e61d5e731417aa3ae
Benjamin Martin <000953368@mohawkcollege.ca>
2024-10-05 17:18:10 -0400

    Revert "Complete interactive windows"
    This reverts commit 033c89001a2f8bd4c3e7229ebe5a1088bfcbd1ee.

--- martin-ben-a2-2D-interactive-drawing/Game.cs
+++ martin-ben-a2-2D-interactive-drawing/Game.cs
@@ -18,16 +18,9 @@ namespace Game10003
         Color concreateGrey = new Color(142, 144, 118);
         Color darkGreenBack = new Color(38, 115, 50);
         Color darkGreenFront = new Color(45, 124, 64);
-        Color lightyellow = new Color(226, 255, 54);
-        Color darkGray = new Color(25, 25, 25);
-
-        // Integers to randomize the moon's placement and phase
         int moonLocation = Random.Integer(40, 340);
         int moonPhase = Random.Integer(0, 30);
 
-        // Array for Window Coordinates
-        float[] xWindowCoord = [60, 160, 260];
-        float[] yWindowCoord = [120, 220, 320];
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -36,6 +29,7 @@ namespace Game10003
         {
             Window.SetSize(400, 400);
             Window.SetTitle("Interactive Drawing");
+            
         }
 
         /// <summary>
@@ -43,14 +37,8 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
-            // Getting Mouse X and Y Coordinates
-            float mouseX = Input.GetMouseX();
-            float mouseY = Input.GetMouseY();
 
             // -------- Background --------
-
-            // Set Background Colour 
-            Window.ClearBackground(navyBlue);
 
             // Background Buildings
             Draw.FillColor = Color.Black;
@@ -77,109 +65,25 @@ namespace Game10003
             Draw.Rectangle(20, 80, 360, 30);
 
             // Dark Windows 
-            Draw.FillColor = darkGray;
+            Draw.FillColor = Color.Black;
             for (int index = 0; index < 3; index++)
             {
                 int xOffset = index * 100;
                 Draw.Square(60 + xOffset, 120, 80);
-                Draw.Square(60 + xOffset, 220, 80);
-                Draw.Square(60 + xOffset, 320, 80);        
             }
-
-            // Click each window to make it light up 
-            if (Input.IsMouseButtonDown(0))
-            {
-                // Window Light Colour
-                Draw.FillColor = lightyellow;
-
-                // Left Column Windows
-                if (mouseX >= xWindowCoord[0] && mouseX <= 140)
-                {
-                    // Top Left Window
-                    if (mouseY >= yWindowCoord[0] && mouseY <= 200)
-                    {
-                        Draw.Square(xWindowCoord[0], yWindowCoord[0], 80);
-                    }
-                    // Middle Left Window
-                    else if (mouseY >= yWindowCoord[1] && mouseY <= 300)
-                    {
-                        Draw.Square(xWindowCoord[0], yWindowCoord[1], 80);
-                    }
-                    // Bottom Left Window
-                    else if (mouseY >= yWindowCoord[2] && mouseY <= 400)
-                    {
-                        Draw.Square(xWindowCoord[0], yWindowCoord[2], 80);
-                    }
-                }
-                // Middle Column 
-                else if (mouseX >= xWindowCoord[1] && mouseX <= 240)
-                {
-                    // Top Middle Window
-                    if (mouseY >= yWindowCoord[0] && mouseY <= 200)
-                    {
-                        Draw.Square(xWindowCoord[1], yWindowCoord[0], 80);
-                    }
-                    // Middle Window
-                    else if (mouseY >= yWindowCoord[1] && mouseY <= 300)
-                    {
-                        Draw.Square(xWindowCoord[1], yWindowCoord[1], 80);
-                    }
-                    // Bottom Middle Window 
-                    else if (mouseY >= yWindowCoord[2] && mouseY <= 400)
-                    {
-                        Draw.Square(xWindowCoord[1], yWindowCoord[2], 80);
-                    }
-                }
-                else if ((mouseX >= xWindowCoord[2] && mouseX <= 340))
-                {
-                    // Top Right Window
-                    if (mouseY >= yWindowCoord[0] && mouseY <= 200)
-                    {
-                        Draw.Square(xWindowCoord[2], yWindowCoord[0], 80);
-                    }
-                    // Middle Right Window
-                    else if (mouseY >= yWindowCoord[1] && mouseY <= 300)
-                    {
-                        Draw.Square(xWindowCoord[2], yWindowCoord[1], 80);
-                    }
-                    // Bottom Right Window 
-                    else if (mouseY >= yWindowCoord[2] && mouseY <= 400)
-                    {
-                        Draw.Square(xWindowCoord[2], yWindowCoord[2], 80);
-                    }
-                }
-            }
-
-
-            // Spacebar Input to light up all windows 
-            if (Input.IsKeyboardKeyDown(KeyboardInput.Space))
-            {
-                Draw.FillColor = lightyellow;
-                for (int index = 0; index < 3; index++)
-                {
-                    int xOffset = index * 100;
-                    Draw.Square(60 + xOffset, 120, 80);
-                    Draw.Square(60 + xOffset, 220, 80);
-                    Draw.Square(60 + xOffset, 320, 80);
-                }
-            }
-
-            // Window Lines 
-            Draw.FillColor = Color.Black;
-            Draw.LineSize = 3; 
-            for (int index = 0; index <= 3; index++)
+            for (int index = 0; index < 3; index++)
             {
                 int xOffset = index * 100;
-                int yOffset = index * 100 + 60;
-                // Vertical Lines
-                Draw.Line(100 + xOffset, 120, 100 + xOffset, 200); 
-                Draw.Line(100 + xOffset, 220, 100 + xOffset, 300); 
-                Draw.Line(100 + xOffset, 320, 100 + xOffset, 400);
-                // Horizontal Lines 
-                Draw.Line(60, 100 + yOffset, 140, 100 + yOffset);
-                Draw.Line(160, 100 + yOffset, 240, 100 + yOffset);
-                Draw.Line(260, 100 + yOffset, 340, 100 + yOffset);
+                Draw.Square(60 + xOffset, 220, 80);
             }
+            for (int index = 0; index < 3; index++)
+            {
+                int xOffset = index * 100;
+                Draw.Square(60 + xOffset, 320, 80);
+            }
+
+
+            Window.ClearBackground(navyBlue);
         }
 
         void DrawBush(float x, float y, float radius)
@@ -199,7 +103,7 @@ namespace Game10003
             Draw.Circle(moonLocation, 30, 30);
             Draw.FillColor = navyBlue;
             Draw.Circle(moonLocation - moonPhase, 30, 30);
-
+            
 
         }
     }
